69e415b40c6ec5a72fe61a2fdeac3ceb2c282c2c
Salvador Rodríguez-Gómez <salrodgom@upo.es>
2018-06-24 21:01:31 +0100

    modified:   lib/fff_raspa/INPUT.elastic_constants 	modified:   scan_structures.sh

--- scan_structures.sh
+++ scan_structures.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-nCPUs=26
+nCPUs=8
 for CIFFile in $(ls -rS struc/*.cif) ; do
   structure=$(echo $CIFFile | sed 's/\.cif//g' | sed 's/struc\///g')
   file=$(echo $CIFFile | sed 's/struc\///g')
-  cp struc/$file .
-  bash main.sh $file $nCPUs > salida.${structure}.txt 
+  #if [ ! -d $structure_* ] ; then
+   cp struc/$file .
+   bash main.sh $file $nCPUs > salida.${structure}.txt
+   mv $file struc/dones
+  #elif [ -f $file ] ; then
+  # mv $file struc/dones
+  #fi
 done
 exit 0
